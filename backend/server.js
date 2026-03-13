5af0778d76362267b3c3717e4fd0c011367282d0
Kodon <amirultgr@gmail.com>
2026-03-13 20:55:20 +0700

    clean up

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-// Import Routes
-const chatRoutes = require('./src/routes/chatRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Jadikan folder uploads sebagai static folder agar bisa diakses browser
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Daftarkan Routes
 app.use('/api/chat', chatRoutes);
 
-// Jalankan Server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`✅ Backend ChefBot berjalan di http://localhost:${PORT}`);
